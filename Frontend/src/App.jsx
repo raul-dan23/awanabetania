668419b88cc2a72928d582aa836f6fee8b7ed24f
Macovei Raul Daniel <rlmacovei@gmail.com>
2026-02-21 05:52:37 +0200

    awanabetania 3.0

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,9 +5,9 @@ import AdminDashboard from './AdminDashboard';
 
 // const API_URL = 'http://awana.betania-tm.ro/api';
 //const API_URL = 'http://86.106.170.96:8080/api';
-// const API_URL = 'http://awana.betania-tm.ro/api'; // asta e pentru server
+const API_URL = 'http://awana.betania-tm.ro/api'; // asta e pentru server
 //const API_URL = 'http://localhost:8080/api'; //pentru localhost
-const API_URL = 'http://192.168.1.156:8080/api';
+// const API_URL = 'http://192.168.1.156:8080/api'; // pentru localhost dar pe tel, dar nu uita sa verifici sa fie pe port ok
 
 // ==========================================
 // 1. SPLASH SCREEN
